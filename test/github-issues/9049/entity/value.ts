d41c9b053c5bf7a931d7cdc77d3763dc6bea8b20
sunrabbit123 <qudwls185@naver.com>
2023-02-01 11:54:30 +0900

    initial commit

--- test/github-issues/9049/entity/value.ts
+++
@@ -1,9 +0,0 @@
-import { Column, ObjectID, ObjectIdColumn } from "../../../../src"
-
-export class Value {
-    @ObjectIdColumn()
-    _id?: ObjectID
-
-    @Column({ type: "string" })
-    description: string
-}
